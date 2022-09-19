08a3caab92a160fa367ae7ef53c6db9cadbafa11
Piotr Machera <piotr.mache@gmail.com>
2022-09-19 23:49:55 +0200

    work with react-chart 2

--- src/Components/Pages/Stats/Stats.js
+++ src/Components/Pages/Stats/Stats.js
@@ -1,7 +1,22 @@
-import React from "react";
-import { Bar } from "chart.js";
+import React, { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import "../../../styles/Interface/Pages/Stats/Stats.css";
+
 const Stats = () => {
-  return <h1>Stats</h1>;
+  const [data] = useState(() => {
+    const localData = localStorage.getItem("dateTask");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    console.log("Zamontowany");
+    console.log(data);
+  }, [data]);
+
+  return (
+    <div id="stats">
+      <Doughnut data={data} />
+    </div>
+  );
 };
 
 export default Stats;
